d4369fce2674e7a8584f8555568b43428556d1eb
auroratide <tf.auroratide@gmail.com>
2017-09-22 17:27:26 -0500

    Parse primitives

---
+++ src/arcon.js
@@ -0,0 +1,10 @@
+var parse = function parse(element) {
+  return element;
+};
+
+var arcon = {
+  parse: parse
+};
+
+module.exports = arcon;
+module.exports.default = arcon;
--- src/arcon.spec.js
+++ src/arcon.spec.js
@@ -2,7 +2,26 @@ const expect = require('chai').expect;
 const arcon = require('./arcon');
 
 describe('arcon', () => {
-  it('should work', () => {
-    expect(true).to.be.true;
+
+  describe('parse', () => {
+    it('should return the passed string when given a string', () => {
+      expect(arcon.parse('')).to.equal('');
+      expect(arcon.parse('a string')).to.equal('a string');
+      expect(arcon.parse('ANOTHER STRING')).to.equal('ANOTHER STRING');
+      expect(arcon.parse('String with symboles #$%^.')).to.equal('String with symboles #$%^.');
+    });
+
+    it('should return the passed number when given a number', () => {
+      expect(arcon.parse(0)).to.equal(0);
+      expect(arcon.parse(1)).to.equal(1);
+      expect(arcon.parse(-1)).to.equal(-1);
+      expect(arcon.parse(2.5)).to.equal(2.5);
+    });
+
+    it('should return the passed boolean when given a boolean', () => {
+      expect(arcon.parse(true)).to.be.true;
+      expect(arcon.parse(false)).to.be.false;
+    });
   });
+  
 });
